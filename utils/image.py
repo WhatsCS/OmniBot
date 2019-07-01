09df8b40dc66532dc42bb89ca670f574a5b0bebc
WhatsCS <whatscs@jointheb.org>
2019-07-01 11:42:31 -0700

    removed the class, was pointless. Making everything as async as possible.

--- utils/image.py
+++ utils/image.py
@@ -1,51 +1,37 @@
 """Image manipulation functions go here"""
 
-# Goal is for this to be file type agnostic (within reason) and then from there,
-# figure out what the file is, process it accordingly and then save the thumbnail in the desired area.
+# TODO: Get this working using byte blobs so we can keep everything in memory and never have to save (ideally)
 # This should probably be asyncd because this shit will interrupt a fuck ton.
 
 import os
 from wand.image import Image
 from subprocess import call
+import aiohttp
 
 
-class Thumbify:
+def create_thumb(self, fobject=None, flist=None):
     """
-    This will probably need access to specific things that I can't think of right now.
-    usage: Thumbify(request/file)
+    Resize to 250, then crop and from that do css
+    bullshit to fix it.
     """
 
-    def __init__(self, fo, filename=None):
-        """
-        init fname...
-        """
-        self.fo = fo
-        self.fname = filename
+    with Image(filename=self.fo) as original:
+        with original.clone() as img:
+            img.convert("jpeg")
+            if img.height == img.width:
+                img.transform(resize="150x150")
+            elif img.height > img.width:
+                img.transform(resize="150x")
+                img.crop(
+                    0, int(round((img.height / 2) - 225)), width=150, height=150
+                )
+            else:
+                img.crop(
+                    int(round((img.width / 2) - 225)), 0, width=150, height=150
+                )
+                img.transform(resize="x150")
 
-    def create_thumb(self):
-        """
-        Resize to 250, then crop and from that do css
-        bullshit to fix it.
-        """
-        path, ext = self.fname.rsplit(".")
-        with Image(filename=self.fname) as original:
-            with original.clone() as img:
-                img.convert("jpeg")
-                if img.height == img.width:
-                    img.transform(resize="250x250")
-                elif img.height > img.width:
-                    img.transform(resize="250x")
-                    img.crop(
-                        0, int(round((img.height / 2) - 125)), width=250, height=250
-                    )
-                else:
-                    img.transform(resize="x250")
-                    img.crop(
-                        int(round((img.width / 2) - 125)), 0, width=250, height=250
-                    )
 
-                try:
-                    if os.path.basename(path).rsplit("_")[1] == "thumb":
-                        img.save(filename=self.fname)
-                except:
-                    img.save(filename=f"{path}_thumb.jpg")
+def create_montage(self, img_list):
+    # `$ montage test*.jpg -tile 3x3 -geometry +0+0 montage-test.jpg`
+    pass
