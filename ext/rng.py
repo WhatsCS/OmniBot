a1bb138491dd401e750bf846293d6b610f90f2fb
WhatsCS <whatscs@jointheb.org>
2019-07-02 14:24:32 -0700

    finalizing r34 for now.

--- ext/rng.py
+++ ext/rng.py
@@ -1,13 +1,16 @@
-from random import randrange
+from random import randrange, choice
 
 from discord.ext import commands
+import discord
 import rule34
-
+import aiohttp
+import io
 
 class Randoms(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.images = rule34.Rule34(self.bot.loop)
+        self.img_l = []
 
     @commands.command(aliases=['rtd', 'd'])
     async def roll_the_dice(self, ctx, roll):
@@ -26,9 +29,33 @@ class Randoms(commands.Cog):
 
     @commands.command()
     @commands.is_nsfw()
-    async def r34(self, ctx, query, list=None):
-        await ctx.send(f'{query}; {list}')
-        img_l = await self.images.getImageURLS(query)
+    @commands.cooldown(1, 5)
+    async def r34(self, ctx, query, list=False):
+        # I have code for requesting a list but no clue if going to properly implement
+        self.img_l = await self.images.getImageURLS(query)
+        if self.img_l is None:
+            ctx.send(f'nothing found for {query}')
+            return
+
+        img_blob = b''
+        if list is False:
+            img_c = choice(self.img_l)
+            async with aiohttp.ClientSession() as session:
+                async with session.get(img_c) as resp:
+                    img_blob = io.BytesIO(await resp.read())
+            #file_blob = io.BytesIO(img_blob)
+            fmsg = discord.File(fp=img_blob, filename='test_crop.jpg')
+            async with ctx.typing():
+                await ctx.send(file=fmsg)
+        else:
+            blob_list = []
+            nlist = []
+            for x in range(0, 9):
+                nlist.append(choice(self.img_l))
+            for url in nlist:
+                async with aiohttp.ClientSession() as session:
+                    async with session.get(url) as resp:
+                        blob_list.append(io.BytesIO(await resp.read()))
 
 def setup(bot):
     bot.add_cog(Randoms(bot))
