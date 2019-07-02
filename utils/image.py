3f7c6f09fa353a2e15fe31acc67ee7b43758cfc5
WhatsCS <whatscs@jointheb.org>
2019-07-02 14:23:03 -0700

    fix a couple things and make it work with byte streams rather than files

--- utils/image.py
+++ utils/image.py
@@ -9,29 +9,29 @@ from subprocess import call
 import aiohttp
 
 
-def create_thumb(self, fobject=None, flist=None):
+def create_thumb(fobject=None):
     """
     Resize to 250, then crop and from that do css
     bullshit to fix it.
     """
+    with Image(blob=fobject) as img:
+        img.convert("jpeg")
+        if img.height == img.width:
+            img.transform(resize="150x150")
+        elif img.height > img.width:
+            img.transform(resize="150x")
+            img.crop(
+                0, int(round((img.height / 2) - 225)), width=150, height=150
+            )
+        else:
+            img.crop(
+                int(round((img.width / 2) - 225)), 0, width=150, height=150
+            )
+            img.transform(resize="x150")
+        fobject = img.make_blob('jpg')
+        return fobject
 
-    with Image(filename=self.fo) as original:
-        with original.clone() as img:
-            img.convert("jpeg")
-            if img.height == img.width:
-                img.transform(resize="150x150")
-            elif img.height > img.width:
-                img.transform(resize="150x")
-                img.crop(
-                    0, int(round((img.height / 2) - 225)), width=150, height=150
-                )
-            else:
-                img.crop(
-                    int(round((img.width / 2) - 225)), 0, width=150, height=150
-                )
-                img.transform(resize="x150")
 
-
-def create_montage(self, img_list):
-    # `$ montage test*.jpg -tile 3x3 -geometry +0+0 montage-test.jpg`
-    pass
+# def create_montage(img_list):
+#     # `$ montage test*.jpg -tile 3x3 -geometry +0+0 montage-test.jpg`
+#     pass
